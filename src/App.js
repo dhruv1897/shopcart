2a8376bc7971906dc71638e1d3a19a05c0070f3d
dhruv1897 <66731309+dhruv1897@users.noreply.github.com>
2024-09-07 12:16:24 -0700

    shopcart lab

--- src/App.js
+++ src/App.js
@@ -1,29 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from "react"; 
-import "bootstrap/dist/css/bootstrap.min.css"; 
-import {faShoppingCart} from "@fortawesome/fontawesome-svg-core";
-import {fontAwesomeIcon} from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {
+        UnisexCologne: {
+          name: "Unisex Cologne",
+          quantity: 0,
+          price: 25.99,
+          image:
+            "./products/cologne.jpg",
+        },
+        Appleiwatch: {
+          name: "Apple iWatch",
+          quantity: 0,
+          price: 399.99,
+          image:
+            "./products/iwatch.jpg",
+        },
+        uniquemug: {
+          name: "Unique Mug",
+          quantity: 0,
+          price: 12.99,
+          image:
+            "./products/mug.jpg",
+        },
+        menswallet: {
+          name: "Mens Wallet",
+          quantity: 0,
+          price: 49.99,
+          image:
+          "./products/wallet.jpg",
+        },
+      },
+      cartTotal: 0,
+    };
+  }
+
+  handleQuantityChange = (itemName, quantity) => {
+    const updatedItems = { ...this.state.items };
+    updatedItems[itemName].quantity = quantity;
+
+    this.setState({ items: updatedItems }, () => {
+      this.calculateCartTotal();
+    });
+  };
+
+  calculateCartTotal = () => {
+    let total = 0;
+    for (const item in this.state.items) {
+      total +=
+        this.state.items[item].quantity * this.state.items[item].price;
+    }
+    this.setState({ cartTotal: total });
+  };
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container blue mt-5 col">
+      <h1 className="text-left mb-2">Shop to React</h1>
+      <div>
+          <FontAwesomeIcon icon={faShoppingCart} />
+          <span>
+            {this.state.cartTotal > 0 ? this.state.cartTotal : "0"} items
+          </span>
+      </div>
+      <div>
+        {Object.keys(this.state.items).map((item) => (
+          <div key={item}>
+            <div>
+              <div>
+               <ListGroup>
+               <ListGroupItem>
+                <h5 className="card-title">{this.state.items[item].name}</h5>
+                <img
+                  src={this.state.items[item].image}
+                  alt={this.state.items[item].name}
+                  className="img-fluid"
+                />
+                  <input
+                    type="number"
+                    className="form-control"
+                    min="0"
+                    value={this.state.items[item].quantity}
+                    onChange={(e) =>
+                      this.handleQuantityChange(
+                        item,
+                        parseInt(e.target.value)
+                      )
+                    }
+                  />
+                  <div className="input-group mt-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">Quantity</span>              
+                  </div>
+                </div>
+                </ListGroupItem>
+                </ListGroup> 
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+}
+
 
 export default App;
